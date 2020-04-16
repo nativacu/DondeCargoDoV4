41ba67d3aa074a4fe1bb36536e6149e3de7d1eee
ericksaga <erickmedina09@gmail.com>
2020-04-16 00:41:51 -0300

    added dataHandler services

--- src/app/services/dataHandler/data-handler.service.ts
+++ src/app/services/dataHandler/data-handler.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Cargador } from 'src/app/models/cargador';
+import { CuentaBancaria } from 'src/app/models/cuenta-bancaria';
+import { Cobro } from 'src/app/models/cobro';
+import { IOTPlug } from 'src/app/models/iotplug';
+import { Lugar } from 'src/app/models/lugar';
+import { Pago } from 'src/app/models/pago';
+import { TarjetaCredito } from 'src/app/models/tarjeta-credito';
+import { TransaccionCarga } from 'src/app/models/transaccion-carga';
+import { User } from 'src/app/models/user';
+import { Vehiculo } from 'src/app/models/vehiculo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
-
+  cargador: Cargador;
+  cobro: Cobro;
+  cuentaBancaria: CuentaBancaria;
+  iotPlug: IOTPlug;
+  lugar: Lugar;
+  pago: Pago;
+  tarjetaCredito: TarjetaCredito;
+  transaccionCarga: TransaccionCarga;
+  user: User;
+  vehiculo: Vehiculo;
+  
   constructor() { }
 }
